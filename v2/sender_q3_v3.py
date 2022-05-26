ce7f7dd21686998953bb14795ad5b45e1ac9d789
annabechang <31103470+annabechang@users.noreply.github.com>
2022-05-25 23:07:03 -0700

    Update sender_q3_v3.py

--- v2/sender_q3_v3.py
+++ v2/sender_q3_v3.py
@@ -92,7 +92,7 @@ def receive_acknowledgements(sockt,window_start):
 	while current_timeouts < consecutive_receive_timeouts:
 		try:
 			sockt.setblocking(0)
-			sockt.settimeout(0.02)
+			sockt.settimeout(0.1)
 
 			ack = sockt.recv(BUFFER_SIZE)
 			# sockt.setblocking(1)
